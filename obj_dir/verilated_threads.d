e5fc2a388a9d35bb7de25a3fb68b88352ddb4432
MythriReddy <22011p0411@gmil.com>
2026-07-12 20:23:18 +0000

    Clean up junk swap files and obj_dir

--- obj_dir/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
- /apps/vlsi/tools/verilator/5.016/share/verilator/include/verilated_threads.cpp \
- /apps/vlsi/tools/verilator/5.016/share/verilator/include/verilatedos.h \
- /apps/vlsi/tools/verilator/5.016/share/verilator/include/verilated_threads.h \
- /apps/vlsi/tools/verilator/5.016/share/verilator/include/verilated.h \
- /apps/vlsi/tools/verilator/5.016/share/verilator/include/verilated_config.h \
- /apps/vlsi/tools/verilator/5.016/share/verilator/include/verilated_types.h \
- /apps/vlsi/tools/verilator/5.016/share/verilator/include/verilated_funcs.h
--- obj_dir/verilated_timing.d
+++
@@ -1,8 +0,0 @@
-verilated_timing.o: \
- /apps/vlsi/tools/verilator/5.016/share/verilator/include/verilated_timing.cpp \
- /apps/vlsi/tools/verilator/5.016/share/verilator/include/verilated_timing.h \
- /apps/vlsi/tools/verilator/5.016/share/verilator/include/verilated.h \
- /apps/vlsi/tools/verilator/5.016/share/verilator/include/verilatedos.h \
- /apps/vlsi/tools/verilator/5.016/share/verilator/include/verilated_config.h \
- /apps/vlsi/tools/verilator/5.016/share/verilator/include/verilated_types.h \
- /apps/vlsi/tools/verilator/5.016/share/verilator/include/verilated_funcs.h
